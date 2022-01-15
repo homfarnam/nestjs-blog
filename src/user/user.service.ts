1c70312bbbf141edf83650c5af04829e8e8ba547
homfarnam <homfarnam@gmail.com>
2022-01-16 00:36:24 +0330

    Create user services

---
+++ src/user/user.service.ts
@@ -0,0 +1,34 @@
+import { User } from './models/user.interface';
+import { UserEntity } from './models/user.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { from, Observable } from 'rxjs';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  create(user: User): Observable<User> {
+    return from(this.userRepository.save(user));
+  }
+
+  findAll(): Observable<User[]> {
+    return from(this.userRepository.find());
+  }
+
+  findOne(id: number): Observable<User> {
+    return from(this.userRepository.findOne({ id }));
+  }
+
+  deleteOne(id: number): Observable<any> {
+    return from(this.userRepository.delete(id));
+  }
+
+  updateOne(id: number, user: User): Observable<any> {
+    return from(this.userRepository.update(id, user));
+  }
+}
